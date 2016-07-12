172908b41ffe64b6a9e14f245e02416dd63453a9
lsantiago <lsquinones@gmail.com>
2016-07-12 17:07:50 -0500

    Se agrega atributo nombre a Estudiante

--- src/proyecto2016/Estudiante.java
+++ src/proyecto2016/Estudiante.java
@@ -11,5 +11,13 @@ package proyecto2016;
  * @author Santiago Quiñones
  */
 public class Estudiante {
+    private String nombre;
+    private String apellido;
 
+    public Estudiante(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
 }
